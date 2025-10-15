c9ff6947414bbc89b348309fb4076f3185f66b96
youngnam81-kim <kimyn81@naver.com>
2025-10-15 16:37:05 +0900

    .

---
+++ src/main/java/com/example/demo/dto/CalPostRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+
+public class CalPostRequest {
+	private String number1;
+	private String number2; 
+	private String operator;
+	
+	public String getNumber1() {
+		return number1;
+	}
+	public void setNumber1(String number1) {
+		this.number1 = number1;
+	}
+	public String getNumber2() {
+		return number2;
+	}
+	public void setNumber2(String number2) {
+		this.number2 = number2;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+}
---
+++ src/main/java/com/example/demo/dto/CalResponse.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public record CalResponse(
+		String number1, 
+		String number2, 
+		String operator,
+		String result) {
+	
+}
