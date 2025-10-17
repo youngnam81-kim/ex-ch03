e7032075bd09a03c8f5928447d658e4515e7a671
youngnam81-kim <kimyn81@naver.com>
2025-10-17 09:39:50 +0900

    .

--- src/main/java/com/example/demo/controller/SimpleRestController.java
+++ src/main/java/com/example/demo/controller/SimpleRestController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
 
-	@RequestMapping("/article")
-	public String createArticle(@RequestParam("title") String title, @RequestParam("content") String content){
-		return title+"/"+content;
+	@GetMapping("/article")
+	public String createArticle(@RequestParam(value ="title", defaultValue = "타이틀 입니다.") String title, @RequestParam(value = "content", defaultValue = "콘텐츠 입니다.") String content){
+		return title+"<br><strong>"+content;
 	}
 }
